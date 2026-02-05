e372f8c17b39d84239726d9e9d3d5d90ba1c2ec8
Jeff <jdanhutch@gmail.com>
2026-02-04 22:15:18 -0700

    Views, templates, asset bundles

--- src/Web/HomePage/template.php
+++ src/Web/HomePage/template.php
@@ -16,7 +16,15 @@ $this->setTitle($applicationParams->name);
 <div class="text-center">
     <h1>Hello!</h1>
 
-    <p>Let's start something great with <strong>Yii3</strong>!</p>
+    <p>My name is Jeff, a software developer with 8 years of experience using <strong>Yii1</strong>!</p>
+    <p>Since <strong>Yii3</strong> released, I've been exploring the new framework and sharing my experience.</p>
+    <p>I look forward to learning together along the way!</p>
+
+    <ul class="inline-block text-left">
+        <?php foreach ($myCustomParams->thingsToLearn as $thingToLearn): ?>
+        <li><?= $thingToLearn ?></li>
+        <?php endforeach; ?>
+    </ul>
 
     <p>
         <a href="https://github.com/yiisoft/docs/tree/master/guide/en" target="_blank" rel="noopener">
